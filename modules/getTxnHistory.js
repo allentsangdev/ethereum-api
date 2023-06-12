679b577b2bc3bc03e922f9960dde60bc8495664f
allentsangdev <allentsang04@gmail.com>
2023-06-12 00:23:33 -0400

    switch to axios

--- modules/getTxnHistory.js
+++ modules/getTxnHistory.js
@@ -1,4 +1,4 @@
-import fetch from  "node-fetch"
+const axios = require('axios');
 const etherscanEndpoints = {
     mainnet: "https://api.etherscan.io/",
     goerli: "https://api-goerli.etherscan.io/",
@@ -9,12 +9,12 @@ const etherscanApiKey = process.env.ETHERSCAN_API_KEY;
 // Get a list of 'Normal' Transactions By Address
 
 const getTxnHistory = async (address) => {
-    const endpoint = `https://api-sepolia.etherscan.io/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=${etherscanApiKey}`;
+    const endpoint = `${etherscanEndpoints.sepolia}/api?module=account&action=txlist&address=${address}&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=${etherscanApiKey}`;
 
-    const response = await fetch(endpoint);
-    const history = await response.json();
+    const response = await axios.get(endpoint);
+    const history =  response.data;
 
-    return history;
+    return console.log(history) ;
 };
 
 module.exports = {
@@ -22,4 +22,4 @@ module.exports = {
   }
 
 //let address = "0x686a0430dec8E9f2473cA97D7A5735047757400F";
-//getHistoty(address);
+//getTxnHistory(address);
